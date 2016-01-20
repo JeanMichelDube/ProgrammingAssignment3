c54342121b6fec4ef4f4b3a76d7fbd3c0010c392
Jean-Michel Dubé <jean.michel.dube@hotmail.fr>
2016-01-20 16:45:00 -0500

    best.R termine!

--- best.R
+++ best.R
@@ -3,11 +3,11 @@ best <- function(state, outcome) {
   data <- read.csv("outcome-of-care-measures.csv")
   
   ## Check that state and outcome are valid
-  
-  if (outcome == "heart attack"){o=11} else if (outcome == "heart failure") {o=17} else if (outcome == "pneumonia") {o=23} else {stop(best)}
+  if (is.element(as.character(state),data$State)>0) {selectState <- data[data$State == state, ]} else {stop("invalid state")}
+  if (outcome == "heart attack"){o=11} else if (outcome == "heart failure") {o=17} else if (outcome == "pneumonia") {o=23} else {stop("invalid outcome")}
   
   ## Return hospital name in that state with lowest 30-day death rate
-  selectState <- data[data$State == state, ]
+  #selectState <- data[data$State == state, ]
   m <- min(as.vector(selectState[,o]), na.rm = TRUE)
   s <- subset(selectState,as.vector(selectState[,o])==m,Hospital.Name)
   sort(s$Hospital.Name)
